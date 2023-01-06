982e48a24eee3db437c6511424011ea12724228e
Ali Suleiman <allysuley@gmail.com>
2023-01-06 19:04:46 +0300

    Improve UI

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f17941746e709126e87f697497f5a36c",
-"index.html": "7fe2dfb3d28fa83201d135d3eb8fa6de",
-"/": "7fe2dfb3d28fa83201d135d3eb8fa6de",
+"index.html": "ca4af24c94d4ce5cb961194070321851",
+"/": "ca4af24c94d4ce5cb961194070321851",
 "main.dart.js": "f95ce8ae8da32016ef8ce6825e7cc2f5",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "0cbaae00fdf3af44e7881e8d0ca5a0b2",
+"icons/apple-touch-icon.png": "36814f629e5f10658ee0d28755998a11",
 "icons/Icon-192.png": "640fadbe7b856e4ea64d63ff33caf6cb",
 "icons/Icon-512.png": "54ace76f2c7871947fd83a3065d5f5ba",
 "manifest.json": "7bd68b7a7ed986327f875b57dec7193a",
